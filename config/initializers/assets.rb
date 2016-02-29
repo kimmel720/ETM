1703d0c674dc085ca0a610948d68ce245cbb9220
Daniela Dimitrova <ddimitr@brandeis.edu>
2016-02-29 16:16:13 -0500

    bootstrap things

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( nike.jpg )
+
+Rails.application.config.assets.precompile += %w( etm.css )
