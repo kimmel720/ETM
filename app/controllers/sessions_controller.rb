3cb4e4e6daa9ece937533ff29bfad22f2220df9c
Killian Widdis <killian.widdis@gmail.com>
2016-03-07 12:46:08 -0500

    fixed login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_name(params[:name])
-      if user and user.authenticate(params[:password])
+      if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to admin_url
+        redirect_to root_url
       else
         redirect_to login_url, alert: "Invalid user/password combination"
     end
