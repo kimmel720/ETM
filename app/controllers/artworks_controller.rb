50c2f603756288870b7caf0407028c7ada620ec7
Killian Widdis <killian.widdis@gmail.com>
2016-03-17 16:58:14 -0400

    updated artworks controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,6 @@
 class ArtworksController < FrontEndController
   before_action :set_artwork, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authorize, only: [:show]
+  skip_before_action :authorize, only: [:show, :index]
 
   # GET /artworks
   # GET /artworks.json
