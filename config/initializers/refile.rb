fe86657e7d9a734f888d2af94c849b0804c7c8ee
kimmel720 <kimmel720@gmail.com>
2016-03-20 17:09:53 -0400

    refile working again hopefully

---
+++ config/initializers/refile.rb
@@ -0,0 +1,11 @@
+require "refile/s3"
+
+aws = {
+  access_key_id: "AKIAIYIKVWZTV265K65Q",
+  secret_access_key: "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",
+  region: "US-Standard",
+  bucket: "etm-photo-lib"
+}
+
+Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+Refile.store = Refile::S3.new(prefix: "store", **aws)
