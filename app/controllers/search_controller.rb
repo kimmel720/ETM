19fb01fd57d7fee317770012c4c3c1730b54af11
Gabriel Seltzer <gabeseltzer@gmail.com>
2016-04-07 16:04:44 -0400

    search links working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,29 @@
 class SearchController < ApplicationController
   def results
     @search_term = params[:search].html_safe
+
     @results = {
-      "Artists" => Artist.search(@search_term),
-      "Artworks" => Artwork.search(@search_term),
-      "Exhibitions" => Exhibition.search(@search_term),
-      "Museums" => Museum.search(@search_term)
+      "Artists" =>
+          Artist.search(@search_term).each.map{ |artist| {
+            name: artist.name,
+            path: artist_path(artist)
+          }},
+      "Artworks" =>
+        Artwork.search(@search_term).each.map{ |artwork| {
+          name: artwork.name,
+          path: museum_exhibition_artwork_path(artwork.exhibition.museum, artwork.exhibition, artwork)
+        }},
+      "Exhibitions" =>
+        Exhibition.search(@search_term).each.map{ |exhibition| {
+          name: exhibition.name,
+          path: museum_exhibition_path(exhibition.museum, exhibition)
+        }},
+      "Museums" =>
+        Museum.search(@search_term).each.map{ |museum| {
+          name: museum.name,
+          path: museum_path(museum)
+        }}
+
      }
      @any_results = false
      @results.each do |key, value|
