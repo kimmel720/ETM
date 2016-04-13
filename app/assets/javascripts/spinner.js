ea9166461cf61fb5fd8920c25bc2392aaf836e6c
Gabriel Seltzer <gabeseltzer@gmail.com>
2016-04-12 23:32:40 -0400

    got loading screen working!

--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -1,14 +1,31 @@
 $( document ).ready(function() {
 
   // hide spinner
-  $(".loading-icon").hide();
-
+  $(".loading-container").addClass('animated fadeOut');
+  $(".loading-container").delay(900).hide(0);
+  $('a').on('click', function(){
+    $(".loading-container").show();
+    $(".loading-container").removeClass('animated fadeOut');
+    $(".loading-container").addClass('animated fadeIn');
+  });
 
 });
-$(document).on("page:fetch", function(){
-  $(".loading-icon").show();
-});
+// $(document).on("page:fetch", function(){
+//   // $(".loading-container").show();
+//   $(".loading-container").className += 'animated fadeIn';
+// });
 
-$(document).on("page:receive", function(){
-  $(".loading-icon").hide(0);
+// $(document).on("page:receive", function(){
+//   $(".loading-container").addClass('animated fadeOut');
+  // $(".loading-container").hide();
+
+// });
+document.addEventListener('page:change', function() {
+  $(".loading-container").removeClass('animated fadeIn');
+  $(".loading-container").addClass('animated fadeOut');
+});
+document.addEventListener('page:fetch', function() {
+  $(".loading-container").show();
+  $(".loading-container").removeClass('animated fadeOut');
+  $(".loading-container").addClass('animated fadeIn');
 });
